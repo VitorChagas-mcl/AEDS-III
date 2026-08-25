929c486e784f5847d1f412fba0da20af56dbf068
Vitor Chagas <vitorcjagas@gmail.com>
2026-08-25 18:48:53 -0300

    feature: Entidade Usuario

---
+++ AEDS III/TPS/TP1/src/entidades/Usuario.java
@@ -0,0 +1,84 @@
+package entidades;
+
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
+public class Usuario {
+    private int idUsuario;
+    private String nome;
+    private String email;
+    private int hashSenha;
+    private int hashRespostaSecreta;
+    private String perguntaSecreta;
+    
+
+    public Usuario(){
+        this(-1, "", "", 0, "", 0);
+    }
+
+    public Usuario(int idUsuario, String nome, String email, int hashSenha, String perguntaSecreta, int hashRespostaSecreta){
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+        this.email = email;
+        this.hashSenha = hashSenha;
+        this.perguntaSecreta = perguntaSecreta;
+        this.hashRespostaSecreta = hashRespostaSecreta;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getHashSenha() {
+        return hashSenha;
+    }
+
+    public void setHashSenha(String hashSenha) {
+        this.hashSenha = hashSenha.hashCode();
+    }
+
+    public String getPerguntaSecreta() {
+        return perguntaSecreta;
+    }
+
+    public void setPerguntaSecreta(String perguntaSecreta) {
+        this.perguntaSecreta = perguntaSecreta;
+    }
+
+    public int getHashRespostaSecreta() {
+        return hashRespostaSecreta;
+    }
+
+    public void setHashRespostaSecreta(String hashRespostaSecreta) {
+        String resposataNormalizada = tirarAcentos(hashRespostaSecreta);
+        this.hashRespostaSecreta = resposataNormalizada.toLowerCase().trim().hashCode();
+    }
+
+    public static String tirarAcentos(String texto) { 
+        if (texto == null) { 
+            return null; 
+        } 
+        String normalizado = Normalizer.normalize( texto, Normalizer.Form.NFD ); 
+        return Pattern.compile("\\p{InCombiningDiacriticalMarks}+").matcher(normalizado).replaceAll(""); 
+    }
+}
