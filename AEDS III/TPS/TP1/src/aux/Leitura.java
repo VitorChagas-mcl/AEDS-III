a808fc02e6273e99dc22dee64e0c8da4878e4343
Vitor Chagas <vitorcjagas@gmail.com>
2026-08-27 10:10:02 -0300

    rm: main MenuUser

---
+++ AEDS III/TPS/TP1/src/aux/Leitura.java
@@ -0,0 +1,15 @@
+package aux;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Leitura {
+    private static final Scanner scan = new Scanner(System.in);
+    public static String LeituraTeclado(){
+        try{
+            return scan.hasNextLine() ? scan.nextLine() : "";
+        }catch(NoSuchElementException e){
+            return "";
+        }
+    }
+}
