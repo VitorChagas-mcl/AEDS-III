79f954c388d6e988d1d855a795710af881f0fe4c
Vitor Chagas <vitorcjagas@gmail.com>
2026-09-05 09:47:49 -0300

    feat: Tela Inicio

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,3 +1,4 @@
+import auxiliar.Leitura;
 import tela.MenuUser;
 import entidades.Usuario;
 public class Main{
@@ -11,6 +12,35 @@ public class Main{
                 return;
             }
 
+            String op;
+            do{
+                System.out.println("\n\nAJUDA AÍ 1.0");
+                System.out.println("------------");
+                System.out.println("\n > Inicio");
+                System.out.println("\n(A) Minha área");
+                System.out.println("(B) Buscar perguntas");
+                System.out.println("\n(S) Sair");
+                System.out.print("\nOpção: ");
+
+                op = Leitura.Teclado().trim().toUpperCase();
+                switch(op){
+                    case "A":
+                        System.out.println("Tela do Usuario");
+                        break;
+
+                    case "B":
+                        System.out.println("Tela de Perguntas");
+                        break;
+
+                    case "S":
+                        System.out.println("Até logo...");
+                        return;
+
+                    default:
+                        System.out.println("Digite uma opção valida");
+                        break;
+                }
+            }while(!op.equalsIgnoreCase("S"));
         }catch(Exception e){
             System.out.println("Erro do sistema: " + e);
             e.printStackTrace();
